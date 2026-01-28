14378e8c872d956903c4b5301ae8a8207bfb24f6
Oscar G. Vizcaino Monroy <41452685+Angelcaido37@users.noreply.github.com>
2026-01-28 09:34:25 -0700

    Delete src directory

--- src/Redes Neuronales/MiniProyectos/prueva.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("CUDA disponible:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("Nombre del dispositivo:", torch.cuda.get_device_name(0))
-    print("Capacidad de cómputo:", torch.cuda.get_device_capability(0))
